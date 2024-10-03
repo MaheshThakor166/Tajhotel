f6ea2907f50f5b1ebb69fa393148251c0bcb0b78
Mahesh Thakor <bcasemmaheshthakor@gmail.com>
2024-10-03 10:38:31 +0530

    booking updated successfully

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', function () {
 
 Route::view('register','auth.register')->name('register');
 Route::view('login','auth.login')->name('login');
+Route::view('booking','booking')->name('booking');
 
 Route::post('registerSave',[UserController::class,'register'])->name('registerSave');
 
